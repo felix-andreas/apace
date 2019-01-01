e4bd7c02fdfd24b2ec422e632f49c2130c22afad
felix <felix>
2019-01-02 00:41:25 +0100

    Initial Commit

--- examples/twissfunctions-FODO-lattice.py
+++ examples/twissfunctions-FODO-lattice.py
@@ -12,7 +12,7 @@ Q1 = el.Quad('Q1', length=0.2, k1=1.2)
 B1 = el.Bend('B1', length=1.5, angle=0.392701, e1=0.1963505, e2=0.1963505)
 Q2 = el.Quad('Q2', length=0.4, k1=-1.2)
 fodo = el.Line('fodo-cell', [Q1, D1, B1, D1, Q2, D1, B1, D1, Q1])
-ring = el.Mainline('fodo-ring', [fodo])
+ring = el.Mainline('fodo-ring', [fodo] * 8)
 
 lin = LinBeamDyn(ring)
 import matplotlib.pyplot as plt
