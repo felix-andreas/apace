c533397433348747ed0ad12c337c31d2bf04a8f6
felix <felix.andreas95@googlemail.com>
2019-10-08 23:40:28 +0200

    run pytest on push

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,15 +1,15 @@
 import elements as el
-import os
 from elements.plotting import plot_lattice
 
-file_path = os.path.expanduser(f'{el.ROOT_DIR}/../examples/lattices/FODO-lattice.json')
-line = el.read_lattice_file_json(file_path)
-fodo = line.cells["fodo"]
-print(fodo.length)
-Q1 = line.elements["Q1"]
-B1 = line.elements["B1"]
-lin = el.LinBeamDyn(line)
+def test_plot():
+    file_path = 'tests/data/lattices/FODO-lattice.json'
+    line = el.read_lattice_file_json(file_path)
+    fodo = line.cells['fodo']
+    print(fodo.length)
+    Q1 = line.elements['Q1']
+    B1 = line.elements['B1']
+    lin = el.LinBeamDyn(line)
 
-plot_lattice(lin.get_twiss(), line, eta_x_scale=1, path="out.pdf")
+    plot_lattice(lin.get_twiss(), line, eta_x_scale=1, path='out.pdf')
 
 
