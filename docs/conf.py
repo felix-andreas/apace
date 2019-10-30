78fa397b958f4bd612d1216a058f28689bcf3ae0
felix <felix.andreas95@googlemail.com>
2019-10-31 00:12:43 +0100

    use parent directory

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 from apace import __name__, __version__, __author__, __copyright__
 
 # -- Project information -----------------------------------------------------
