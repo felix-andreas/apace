5af16e12547afb236210c6978b2f96563bf5cbc6
felix <felix.andreas95@googlemail.com>
2019-10-30 02:08:00 +0100

    update version number

--- apace/__about__.py
+++ apace/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'apace'
 __description__ = 'Another Particle Accelerator Code'
 __url__ = 'https://github.com/andreasfelix/apace'
-__version__ = '0.1'
+__version__ = '0.0.0'
 __author__ = 'Felix Andreas'
 __license__ = 'GNU General Public License v3.0'
 __copyright__ = 'Copyright 2019 Felix Andreas'
