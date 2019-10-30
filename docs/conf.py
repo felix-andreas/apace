d76a93836c47731f83dc6f79d748420250635d39
felix <felix.andreas95@googlemail.com>
2019-10-31 00:13:59 +0100

    import from __about__

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-from apace import __name__, __version__, __author__, __copyright__
+from apace.__about__ import __name__, __version__, __author__, __copyright__
 
 # -- Project information -----------------------------------------------------
 
