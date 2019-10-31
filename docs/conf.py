45208d23081df0ad3ff2b0be61ef6c7997d7ac64
felix <felix.andreas95@googlemail.com>
2019-10-31 02:48:04 +0100

    add requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 # html_theme = 'sphinx_rtd_theme'
 html_theme = 'press'
+# html_theme = 'sphinx_typlog_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
