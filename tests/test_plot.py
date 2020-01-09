0d9b948ea70850ec006c24094556ac70c1f14be6
Felix Andreas <felix.andreas95@googlemail.com>
2020-01-09 20:47:19 +0100

    update tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,5 +1,6 @@
 import apace as ap
-from apace.plot import plot_lattice
+from apace.plot import plot_twiss, draw_elements, draw_sub_lattices
+import matplotlib.pyplot as plt
 
 import os
 
@@ -8,9 +9,10 @@ file_path = os.path.join(dir_name, "data", "lattices", "fodo_ring.json")
 
 
 def test_plot():
-    line = ap.load_lattice(file_path)
-    twiss = ap.Twiss(line)
+    lattice = ap.load_lattice(file_path)
+    twiss = ap.Twiss(lattice)
+    fig, _ = plot_twiss(twiss)
+    draw_elements(lattice)
+    draw_sub_lattices(lattice)
+    fig.savefig("/tmp/out.pdf")
 
-    plot_lattice(
-        twiss, line, eta_x_scale=1, path=os.path.join(dir_name, "/tmp/out.pdf")
-    )
