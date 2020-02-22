29156ed219c4f78caa886a07b3e8859a1485e6a9
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-22 16:37:45 +0100

    Use pytest's fixtures for tests (#62)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,10 @@
-from pathlib import Path
 from click.testing import CliRunner
 from apace.cli import cli
 
-BASE_PATH = Path(__file__).resolve().parent
-FODO_PATH = BASE_PATH / "data/lattices/fodo_ring.json"
 
-
-def test_twiss(tmp_path):
+def test_twiss(tmp_path, lattice_path):
     runner = CliRunner()
-    out_path = tmp_path / "test_twiss.pdf"
-    result = runner.invoke(cli, ["twiss", str(FODO_PATH), "--output", out_path])
+    input_path = str(lattice_path / "fodo_ring.json")
+    output_path = tmp_path / "test_twiss.pdf"
+    result = runner.invoke(cli, ["twiss", input_path, "--output", output_path])
     assert result.exit_code == 0
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,47 +1,39 @@
-import os
-
 import apace as ap
-
-BASE_PATH = os.path.dirname(__file__)
-FILE_PATH = os.path.join(BASE_PATH, "data", "lattices", "fodo_ring.json")
-
 
 # TODO: fix linter/mypy erros
 # https://stackoverflow.com/questions/59187502/mypy-dict-containing-instances-of-different-subclasses
-def test_attributes():
-    fodo = ap.Lattice.from_file(FILE_PATH)
-    d1 = fodo["D1"]
+def test_attributes(fodo_ring):
+    d1 = fodo_ring["D1"]
     assert isinstance(d1, ap.Drift)
-    assert 0.55 == fodo["D1"].length
+    assert 0.55 == fodo_ring["D1"].length
 
-    b1: ap.Dipole = fodo["B1"]
+    b1: ap.Dipole = fodo_ring["B1"]
     assert isinstance(b1, ap.Dipole)
-    assert 0.392701 == fodo["B1"].angle
-    assert 0.1963505 == fodo["B1"].e1
-    assert 0.1963505 == fodo["B1"].e2
-    assert 1.5 == fodo["B1"].length
+    assert 0.392701 == fodo_ring["B1"].angle
+    assert 0.1963505 == fodo_ring["B1"].e1
+    assert 0.1963505 == fodo_ring["B1"].e2
+    assert 1.5 == fodo_ring["B1"].length
 
-    q1 = fodo["Q1"]
+    q1 = fodo_ring["Q1"]
     assert isinstance(q1, ap.Quadrupole)
-    assert 0.2 == fodo["Q1"].length
-    assert 1.2 == fodo["Q1"].k1
+    assert 0.2 == fodo_ring["Q1"].length
+    assert 1.2 == fodo_ring["Q1"].k1
 
-    q2 = fodo["Q2"]
+    q2 = fodo_ring["Q2"]
     assert isinstance(q2, ap.Quadrupole)
-    assert 0.4 == fodo["Q2"].length
-    assert -1.2 == fodo["Q2"].k1
+    assert 0.4 == fodo_ring["Q2"].length
+    assert -1.2 == fodo_ring["Q2"].k1
 
-    fodo_cell = fodo["FODO"]
+    fodo_cell = fodo_ring["FODO"]
     assert isinstance(fodo_cell, ap.Lattice)
     assert 6.0 == fodo_cell.length
 
-    assert isinstance(fodo, ap.Lattice)
-    assert 48 == fodo.length
+    assert isinstance(fodo_ring, ap.Lattice)
+    assert 48 == fodo_ring.length
 
 
-def test_serialize_lattice():
-    fodo = ap.Lattice.from_file(FILE_PATH)
-    fodo_reload = ap.Lattice.from_dict(fodo.as_dict())
-    assert fodo.length == fodo_reload.length
-    assert len(fodo.elements) == len(fodo_reload.elements)
-    assert len(fodo.sub_lattices) == len(fodo_reload.sub_lattices)
+def test_serialize_lattice(fodo_ring):
+    fodo_reload = ap.Lattice.from_dict(fodo_ring.as_dict())
+    assert fodo_ring.length == fodo_reload.length
+    assert len(fodo_ring.elements) == len(fodo_reload.elements)
+    assert len(fodo_ring.sub_lattices) == len(fodo_reload.sub_lattices)
