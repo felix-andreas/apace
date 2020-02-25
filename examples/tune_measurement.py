5a985cf887fc917b45ba8faf42c39a6548bc32f2
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-26 00:18:36 +0100

    push all

--- examples/tune_measurement.py
+++ examples/tune_measurement.py
@@ -9,6 +9,7 @@ Get tune from Fourier-Transform of transversal particle oscillation at fixed pos
 # Some imports ...
 
 import numpy as np
+from pathlib import Path
 from scipy.fftpack import fft
 import apace as ap
 import matplotlib.pyplot as plt
@@ -17,24 +18,25 @@ from math import sqrt
 #%%
 # Load FODO lattice from file
 
-fodo = ap.Lattice.from_file("../data/lattices/fodo_ring.json")
+path = Path(__file__).resolve().parent / "../data/lattices/fodo_ring.json"
+fodo = ap.Lattice.from_file(path)
 
 #%%
 # Create particle distribution
 
 n_particles = 5
-n_turns = 20
+n_turns = 50
 position = 0
-particles = ap.distribution(n_particles, x_dist="uniform", x_width=0.02, x_center=0.01)
+dist = ap.distribution(n_particles, x_dist="uniform", x_width=0.002, x_center=0.001)
 
-matrix_tracking = ap.MatrixTracking(fodo, particles, turns=n_turns, positions=0)
+matrix_tracking = ap.MatrixTracking(fodo, dist, turns=n_turns, watch_points=[0])
 
 #%%
 # Plot x-x' phase space
 plt.subplot(2, 2, 1)
 
 for i in range(n_particles):
-    plt.plot(matrix_tracking.x[:, i], matrix_tracking.y[:, i], "o")
+    plt.plot(matrix_tracking.x[:, i], matrix_tracking.x_dds[:, i], "o")
 
 plt.xlabel("x / m")
 plt.ylabel("x'")
@@ -56,8 +58,8 @@ plt.plot(matrix_tracking.orbit_position, matrix_tracking.x[:, -1], "rx")
 plt.xlabel(f"orbit position / s")
 plt.ylabel(f"horizontal offset x at fixed position {position} / m")
 
-# # Plot horizontal offset for multiple positions
-matrix_tracking_all_positions = ap.MatrixTracking(fodo, particles, turns=n_turns)
+# Plot horizontal offset for multiple positions
+matrix_tracking_all_positions = ap.MatrixTracking(fodo, dist, turns=n_turns)
 
 plt.subplot(2, 2, 4)
 plt.plot(
@@ -71,4 +73,4 @@ plt.ylabel("horizontal offset for all positions / m")
 
 
 plt.gcf().set_size_inches(16, 8)
-plt.tight_layout()
+plt.show()
