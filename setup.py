5cec2c1f09a18b9e96000baf39f56603f034d7c0
Felix Andreas <felix.andreas95@googlemail.com>
2020-09-01 12:42:58 +0000

    Automated Code Formatting using Black

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(
     author=about["__author__"],
     license=about["__license__"],
     packages=find_packages(),
-    package_data={"data": ["data"],},
+    package_data={
+        "data": ["data"],
+    },
     install_requires=[
         "latticejson>=0.1.2",
         "numpy",
@@ -28,7 +30,9 @@ setup(
     ],
     test_requires=["pytest"],
     python_requires=">=3.6",
-    cffi_modules=["lib/build.py:ffi_builder",],
+    cffi_modules=[
+        "lib/build.py:ffi_builder",
+    ],
     entry_points={"console_scripts": ["apace=apace.cli:cli"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
