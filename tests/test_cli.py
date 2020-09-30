85081b487942888941721f7375e48d94670842ea
Felix Andreas <felix.andreas95@googlemail.com>
2020-09-30 02:35:13 +0200

    update

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ from apace.cli import cli
 
 def test_twiss(tmp_path, lattice_path):
     runner = CliRunner()
-    input_path = str(lattice_path / "fodo_ring.json")
+    input_path = str(lattice_path / "fodo_cell.json")
     output_path = tmp_path / "test_twiss.pdf"
     result = runner.invoke(cli, ["twiss", input_path, "--output", output_path])
     assert result.exit_code == 0
--- tests/test_matrix_method.py
+++ tests/test_matrix_method.py
@@ -2,10 +2,10 @@ import numpy as np
 import apace as ap
 
 
-def test_angle_k1(fodo_ring):
-    b1 = fodo_ring["B1"]
-    q1 = fodo_ring["Q1"]
-    matrix_method = ap.MatrixMethod(fodo_ring)
+def test_angle_k1(fodo_cell):
+    b1 = fodo_cell["B1"]
+    q1 = fodo_cell["Q1"]
+    matrix_method = ap.MatrixMethod(fodo_cell)
     b1_indices = matrix_method.element_indices[b1]
     q1_indices = matrix_method.element_indices[q1]
     assert np.all(b1.k0 == matrix_method.k0[b1_indices])
