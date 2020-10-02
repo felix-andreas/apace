190eaf7cdb345dfc76bdbda55eeeec793e736fcc
Felix Andreas <felix.andreas95@googlemail.com>
2020-10-02 02:41:38 +0200

    update

--- apace/plot.py
+++ apace/plot.py
@@ -13,6 +13,9 @@ from collections.abc import Iterable
 from .classes import Base, Drift, Dipole, Quadrupole, Sextupole, Octupole, Lattice
 
 
+FONT_SIZE = 8
+
+
 class Color:
     RED = "crimson"
     GREEN = "mediumseagreen"
@@ -32,13 +35,9 @@ ELEMENT_COLOR = {
     Octupole: Color.BLUE,
 }
 
-FONT_SIZE = 8
 
 OPTICAL_FUNCTIONS = {
-    "beta_x": (
-        r"$\beta_x$/m",
-        Color.RED,
-    ),
+    "beta_x": (r"$\beta_x$/m", Color.RED,),
     "beta_y": (r"$\beta_y$/m", Color.BLUE),
     "eta_x": (r"$\eta_x$/m", Color.GREEN),
     "psi_x": (r"$\psi_x$", Color.YELLOW),
@@ -362,11 +361,7 @@ class TwissPlot:
 
         handles, labels = self.fig.axes[0].get_legend_handles_labels()
         self.fig.legend(
-            handles,
-            labels,
-            loc="upper left",
-            ncol=10,
-            frameon=False,
+            handles, labels, loc="upper left", ncol=10, frameon=False,
         )
         self.fig.suptitle(twiss.lattice.name, ha="right", x=0.98)
         self.fig.tight_layout()
